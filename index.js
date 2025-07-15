f686b8c6a8269da295181b0351317a44454b4c5c
AlanMedina53232 <42563570+AlanMedina53232@users.noreply.github.com>
2025-07-15 12:01:07 -0700

    Se aumenta el limite de solcitiudes a la base de datos

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(helmet({
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 1000, // limit each IP to 100 requests per windowMs
   message: {
     error: 'Demasiadas solicitudes desde esta IP, intenta de nuevo más tarde.'
   }
